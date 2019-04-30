2578215447786b58b6d5a2bec131b893fc5332e5
Brian Cox <brian@Brians-MacBook-Air.local>
2019-04-30 14:52:35 -0500

    Fixed errors for basic client threading, removed use of classes

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
-import threading
+import select
+import sys
+from thread import *
 
 # Global variables
 
@@ -7,90 +9,89 @@ MAXCLIENTS = 3
 LOGINFILE = "users.txt"
 SERVERPORT = 12492
 
-# Chat Server class
-class ChatServer:
+server = None
+loginDictionary = {}
+clients = []
 
-    # Initialize class
-    def __init__(self, port):
-        # Set port
-        self.port = port
+# Initialize global variables
+def serverInit(port):
+    global server
+    # Create socket
+    server = socket.socket()
+    server.bind(("", port))
+    server.listen(MAXCLIENTS)
 
-        # Create socket
-        self.s = socket.socket()
-        self.s.bind(("", port))
-        self.s.listen(MAXCLIENTS)
+    # Import logins
+    file = open(LOGINFILE, "r")
+    for line in file:
+        if line != "\n":
+            loginInfo = line.split(",")
+            loginID = loginInfo[0]
+            loginPassword = loginInfo[1].rstrip()
+            loginDictionary[loginID] = loginPassword
+    print loginDictionary
+    file.close()
 
-        # Import logins
-        file = open(LOGINFILE, "r")
-        self.loginDictionary = {}
-        for line in file:
-            if line != "\n":
-                loginInfo = line.split(",")
-                loginID = loginInfo[0]
-                loginPassword = loginInfo[1].rstrip()
-                self.loginDictionary[loginID] = loginPassword
-        print self.loginDictionary
-        file.close()
-
-    # Create a client thread
-    def client(c, addr):
-        print "Got connection from", addr
-        c.send("Welcome to the chat room!")
-        repeat = True
-        while repeat:
-            message = c.recv(1024)
-            print "\"" + message + "\"", "from", addr
-            brokeninput = message.split(" ")
-            try:
-                # LOGOUT REQUEST
-                if brokeninput[0] == "logout":
-                    print "LOGOUT REQUEST FROM", addr
-                    c.close()
-                    repeat = False
-                # LOGIN REQUEST
-                elif brokeninput[0] == "login":
-                    print "LOGIN REQUEST FROM", addr
-                    print brokeninput[0], brokeninput[1], brokeninput[2]
-                    if(self.loginDictionary[str(brokeninput[1])] == brokeninput[2]):
-                        print "SUCCESSFUL LOGIN FROM", addr, "TO USER", brokeninput[1]
-                        self.loginID = brokeninput[1]
-                        c.send("Server: Now logged in to user " + self.loginID + ".")
-                    else:
-                        print "INVALID LOGIN FROM", addr
-                        c.send("Server: Incorrect login.")
-                    repeat = True
-                # SEND REQUEST
-                elif brokeninput[0] == "send":
-                    if self.loginID != "":
-                        c.send(self.loginID + ": " + message[5:])
-                    else:
-                        c.send("Server: Denied. Please login first.")
-                # NEWUSER REQUEST
-                elif brokeninput[0] == "newuser":
-                    self.loginDictionary[str(brokeninput[1])] = brokeninput[2]
-                    self.loginID = brokeninput[1]
-                    file = open(LOGINFILE, "a")
-                    file.write("\n" + brokeninput[1] + "," + brokeninput[2])
-                    file.close()
-                    c.send("Server: New user created. Now logged in to user " + self.loginID + ".")
-                    repeat = True
-                # UNKNOWN REQUEST
+# Create a client thread
+def client(c, addr):
+    print "Thread created for", addr
+    c.send("Welcome to the chat room!")
+    loginID = ""
+    repeat = True
+    while repeat:
+        message = c.recv(1024)
+        print "\"" + message + "\"", "from", addr
+        brokeninput = message.split(" ")
+        try:
+            # LOGOUT REQUEST
+            if brokeninput[0] == "logout":
+                print "LOGOUT REQUEST FROM", addr
+                c.close()
+                repeat = False
+            # LOGIN REQUEST
+            elif brokeninput[0] == "login":
+                print "LOGIN REQUEST FROM", addr
+                print brokeninput[0], brokeninput[1], brokeninput[2]
+                if(loginDictionary[str(brokeninput[1])] == brokeninput[2]):
+                    print "SUCCESSFUL LOGIN FROM", addr, "TO USER", brokeninput[1]
+                    loginID = brokeninput[1]
+                    c.send("Server: Now logged in to user " + loginID + ".")
                 else:
-                    c.send("Server: Invalid request.")
-                    repeat = True
-            except:
+                    print "INVALID LOGIN FROM", addr
+                    c.send("Server: Incorrect login.")
+                repeat = True
+            # SEND REQUEST
+            elif brokeninput[0] == "send":
+                if loginID != "":
+                    c.send(loginID + ": " + message[5:])
+                else:
+                    c.send("Server: Denied. Please login first.")
+            # NEWUSER REQUEST
+            elif brokeninput[0] == "newuser":
+                loginDictionary[str(brokeninput[1])] = brokeninput[2]
+                loginID = brokeninput[1]
+                file = open(LOGINFILE, "a")
+                file.write("\n" + brokeninput[1] + "," + brokeninput[2])
+                file.close()
+                c.send("Server: New user created. Now logged in to user " + loginID + ".")
+                repeat = True
+            # UNKNOWN REQUEST
+            else:
                 c.send("Server: Invalid request.")
+                repeat = True
+        except:
+            c.send("Server: Invalid request.")
 
-    # Run server
-    def run(self):
-        print("Server running")
-        # Start listening
-        while True:
-            c, addr = self.s.accept()
-            start_new_thread(client, (c, addr))
-
+# Run server
+def serverRun():
+    print("Server running")
+    # Start listening
+    while True:
+        c, addr = server.accept()
+        print "Got connection from", addr
+        start_new_thread(client, (c, addr))
 
 # Main program
 
-chatServer = ChatServer(SERVERPORT)
-chatServer.run()
+serverInit(SERVERPORT)
+serverRun()
