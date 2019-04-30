9af43835e3ed55a804a88df8a7fab0f84e007aac
Brian Cox <brian@Brians-MacBook-Air.local>
2019-04-30 14:09:52 -0500

    Rough draft of creating client threads

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-import thread
+import threading
 
 # Global variables
 
@@ -32,59 +32,63 @@ class ChatServer:
         print self.loginDictionary
         file.close()
 
+    # Create a client thread
+    def client(c, addr):
+        print "Got connection from", addr
+        c.send("Welcome to the chat room!")
+        repeat = True
+        while repeat:
+            message = c.recv(1024)
+            print "\"" + message + "\"", "from", addr
+            brokeninput = message.split(" ")
+            try:
+                # LOGOUT REQUEST
+                if brokeninput[0] == "logout":
+                    print "LOGOUT REQUEST FROM", addr
+                    c.close()
+                    repeat = False
+                # LOGIN REQUEST
+                elif brokeninput[0] == "login":
+                    print "LOGIN REQUEST FROM", addr
+                    print brokeninput[0], brokeninput[1], brokeninput[2]
+                    if(self.loginDictionary[str(brokeninput[1])] == brokeninput[2]):
+                        print "SUCCESSFUL LOGIN FROM", addr, "TO USER", brokeninput[1]
+                        self.loginID = brokeninput[1]
+                        c.send("Server: Now logged in to user " + self.loginID + ".")
+                    else:
+                        print "INVALID LOGIN FROM", addr
+                        c.send("Server: Incorrect login.")
+                    repeat = True
+                # SEND REQUEST
+                elif brokeninput[0] == "send":
+                    if self.loginID != "":
+                        c.send(self.loginID + ": " + message[5:])
+                    else:
+                        c.send("Server: Denied. Please login first.")
+                # NEWUSER REQUEST
+                elif brokeninput[0] == "newuser":
+                    self.loginDictionary[str(brokeninput[1])] = brokeninput[2]
+                    self.loginID = brokeninput[1]
+                    file = open(LOGINFILE, "a")
+                    file.write("\n" + brokeninput[1] + "," + brokeninput[2])
+                    file.close()
+                    c.send("Server: New user created. Now logged in to user " + self.loginID + ".")
+                    repeat = True
+                # UNKNOWN REQUEST
+                else:
+                    c.send("Server: Invalid request.")
+                    repeat = True
+            except:
+                c.send("Server: Invalid request.")
+
+    # Run server
     def run(self):
         print("Server running")
-        self.connectionDictionary = {}
         # Start listening
         while True:
             c, addr = self.s.accept()
-            print "Got connection from", addr
-            c.send("Welcome to the chat room!")
-            self.loginID = ""
-            repeat = True
-            while repeat:
-                message = c.recv(1024)
-                print "\"" + message + "\"", "from", addr
-                brokeninput = message.split(" ")
-                try:
-                    # LOGOUT REQUEST
-                    if brokeninput[0] == "logout":
-                        print "LOGOUT REQUEST FROM", addr
-                        c.close()
-                        repeat = False
-                    # LOGIN REQUEST
-                    elif brokeninput[0] == "login":
-                        print "LOGIN REQUEST FROM", addr
-                        print brokeninput[0], brokeninput[1], brokeninput[2]
-                        if(self.loginDictionary[str(brokeninput[1])] == brokeninput[2]):
-                            print "SUCCESSFUL LOGIN FROM", addr, "TO USER", brokeninput[1]
-                            self.loginID = brokeninput[1]
-                            c.send("Server: Now logged in to user " + self.loginID + ".")
-                        else:
-                            print "INVALID LOGIN FROM", addr
-                            c.send("Server: Incorrect login.")
-                        repeat = True
-                    # SEND REQUEST
-                    elif brokeninput[0] == "send":
-                        if self.loginID != "":
-                            c.send(self.loginID + ": " + message[5:])
-                        else:
-                            c.send("Server: Denied. Please login first.")
-                    # NEWUSER REQUEST
-                    elif brokeninput[0] == "newuser":
-                        self.loginDictionary[str(brokeninput[1])] = brokeninput[2]
-                        self.loginID = brokeninput[1]
-                        file = open(LOGINFILE, "a")
-                        file.write("\n" + brokeninput[1] + "," + brokeninput[2])
-                        file.close()
-                        c.send("Server: New user created. Now logged in to user " + self.loginID + ".")
-                        repeat = True
-                    # UNKNOWN REQUEST
-                    else:
-                        c.send("Server: Invalid request.")
-                        repeat = True
-                except:
-                    c.send("Server: Invalid request.")
+            start_new_thread(client, (c, addr))
+
 
 # Main program
 
